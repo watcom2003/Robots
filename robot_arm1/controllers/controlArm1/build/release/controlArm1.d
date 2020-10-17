8934e5ca3638828c6e775dbc2d85cad2df58c1ae
watcom2003 <watcom@hotmail.com>
2020-10-17 11:07:11 +0700

    Add files via upload

---
+++ robot_arm1/controllers/controlArm1/build/release/controlArm1.d
@@ -0,0 +1,9 @@
+build/release/controlArm1.o: controlArm1.cpp \
+ D:\Softwares\Webots\projects\robots\neuronics\ipr\libraries\ipr\IPR.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Softwares/Webots/include/controller/c/webots/types.h \
+ D:\Softwares\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/TouchSensor.hpp
---
+++ robot_arm1/controllers/ipr_cube/build/release/ipr_cube.d
@@ -0,0 +1,8 @@
+build/release/ipr_cube.o: ipr_cube.cpp ../../libraries/ipr/IPR.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Softwares/Webots/include/controller/c/webots/types.h \
+ D:\Softwares\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/PositionSensor.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Softwares\Webots/include/controller/cpp/webots/TouchSensor.hpp
